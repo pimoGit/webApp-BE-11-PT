d72bf498bf78b83a669e456e1ddef5cdc2334a02
Simone <info@simoneicardi.com>
2026-04-30 20:16:07 +0200

    aggiungi env per info

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 // importo il router della risorsa pizze
 const bookRouter = require('./routers/bookRouter');
--- data/db.js
+++ data/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '7bloodYr00T',
-    database: 'books'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
