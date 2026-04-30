95e8ec0913bbed110318339b6f43d7ce062c4dd4
Simone <info@simoneicardi.com>
2026-04-30 20:23:09 +0200

    logica sql per index books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,7 +4,14 @@ const connection = require('../data/db');
 // elenco funzioni relative alle rotte della risorsa pizze
 
 function index(req, res) {
-    res.send("questa è la index dei libri");
+    // prepariamo la query
+    const sql = 'SELECT * FROM books';
+
+    // eseguiamo la query!
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        res.json(results);
+    });
 }
 
 function show(req, res) {
