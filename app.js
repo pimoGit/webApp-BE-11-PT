bd141eca3f96fcae86962af4e80c218cf2739812
Simone <info@simoneicardi.com>
2026-05-02 11:46:28 +0200

    setta ref FE su env

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 // abilitiamo dominio FE
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: process.env.FE_APP
 }));
 
 // importo il router della risorsa pizze
